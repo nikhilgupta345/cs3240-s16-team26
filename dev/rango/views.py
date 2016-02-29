a8ab8346b020c0633e3fb8443bc7c93676681034
Nikhil Gupta <ng3br@virginia.edu>
2016-02-29 17:18:07 -0500

    Initial commit

---
+++ dev/rango/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+	context_dict = {'boldmessage':"I am bold font from the context"}
+
+	return render(request, 'rango/index.html', context_dict)
