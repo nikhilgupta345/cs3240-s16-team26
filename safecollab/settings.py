0521784ef0ae066cf0503d388d9166fcca966830
Nikhil Gupta <nikhil@Nikhils-MacBook-Pro.local>
2016-03-29 11:02:03 -0400

    Added login/registration page as well as Bootstrap and JQuery.

--- safecollab/settings.py
+++ safecollab/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'safecollabapp'
+    'safecollabapp',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -99,5 +99,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_URL = '/static/'
