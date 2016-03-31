7275259d4726af176ccc42d264b6b5055eb4257a
Nikhil Gupta <ng3br@virginia.edu>
2016-03-30 20:00:45 -0400

    Added basic user authentication functionality

--- safecollabapp/views.py
+++ safecollabapp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 def login(request): # Home page and login screen
-    if request.method == 'POST':
+    # Initially empty as we don't know if their password was invalid
+    context_dict = {
+        'login_message': ''
+    }
+    
+    if request.method == 'POST': # Check if they submitted the login form
+
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        print(username)
-        print(password)
-        return render(request, 'login.html')
+        user = authenticate(username=username, password=password) # Check if this username/password combo exists
+        if user is not None:
+            login(request, user) # Log the user in if they do
+        else:
+            context_dict['login_message'] = 'Invalid username and password combination.'
 
-    else:
-        return render(request, 'login.html')
+    # If on site before logging in or they had invalid credentials we send them to login page
+    return render(request, 'login.html', context_dict)
+
+def register(request):
+    return None
