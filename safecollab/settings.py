75441f927f7b6d7547f528ae04d97fd9fcf2cc57
radduffin <rad.duffin@gmail.com>
2016-04-04 09:58:07 -0400

    s/PATH/ROOT

--- safecollab/settings.py
+++ safecollab/settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
