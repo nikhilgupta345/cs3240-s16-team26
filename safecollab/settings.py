8fc8d5bc2782b7336eb0ec4fa5d4a963ec58e2ba
radduffin <rad.duffin@gmail.com>
2016-04-04 10:51:33 -0400

    Fixed additional settings typo

--- safecollab/settings.py
+++ safecollab/settings.py
@@ -14,8 +14,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 import dj_database_url
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 MEDIA_URL = '/media/'
