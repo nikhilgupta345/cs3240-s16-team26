f832b25aceec2d23833ecd51797f12a473f2bffc
radduffin <rad.duffin@gmail.com>
2016-04-24 15:12:52 -0400

    Uncommented Heroku lines in settings.py for deployment fix

--- safecollab/settings.py
+++ safecollab/settings.py
@@ -101,8 +101,8 @@ DATABASES = {
 }
 
 # Extra DB configuration for Heroku
-#db_from_env = dj_database_url.config(conn_max_age=500)
-#DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
